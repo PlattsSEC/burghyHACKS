e546ded80c2706f7050365fe2dfc5125fc3cca7a
Danny <dannyfan@live.com>
2017-09-14 16:33:50 -0400

    fixed user registeration

--- burghyHACKS/forms.py
+++ burghyHACKS/forms.py
@@ -2,7 +2,20 @@ from django.contrib.auth.models import User
 from django import forms
 
 class userForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    username = forms.CharField(max_length=30, required=True, help_text='Pick something fancy. Like coolguy1616.')
+    email = forms.EmailField(max_length=50, required=True, help_text="We'll never share your email with anyone else.")
     password = forms.CharField(widget=forms.PasswordInput)
+
     class Meta:
         model = User
         fields = ['first_name','last_name','username','email','password']
+
+    def __init__(self, *args, **kwargs):
+        super(userForm, self).__init__(*args, **kwargs)
+        # Adds form-control class to the fields for bootstrap
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+        })
--- burghyHACKS/views.py
+++ burghyHACKS/views.py
@@ -5,9 +5,11 @@ from django.contrib.auth import authenticate, login
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views import generic
 from django.views.generic import View
-from .forms import userForm
+#from .forms import userForm
 from burghyhacks.settings import BASE_DIR
+from burghyHACKS.forms import userForm
 import os
+
 
 # Create your views here.
 
@@ -16,16 +18,18 @@ def index(request):
     return HttpResponse(temp.render())
     
 class userFormView(View):
-    form_class = userForm
+    #form_class = userForm
     template_name = os.path.join(BASE_DIR,"burghyHACKS/templates/user_form.html")
     fields = ['first_name','last_name','email','password']
     
     def get(self, request):
-        form = self.form_class(None)
+        #form = self.form_class(None)
+        form = userForm(request.GET)
         return render(request, self.template_name, {"form": form})
     
     def post(self, request):
-        form = self.form_class(request.POST)
+        #form = self.form_class(request.POST)
+        form = userForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             username = form.cleaned_data["username"]
@@ -35,9 +39,10 @@ class userFormView(View):
             password = form.cleaned_data["password"]
             user.set_password(password)
             user.save()
-            
             user = authenticate(username=username,password=password)
             if user:
                 if user.is_active:
                     login(request, user)
                     return redirect('/')
+        else:
+            return HttpResponse('form invalid')
